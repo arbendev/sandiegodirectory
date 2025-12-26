423980102867a4aa36462313f470d76b53d85a49
Arben Mehmetaj <amehmetaj@gmail.com>
2025-12-26 15:18:16 +0100

    homepage map zoom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -158,7 +158,7 @@
             L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
                 subdomains: 'abcd',
-                maxZoom: 10
+                maxZoom: 12
             }).addTo(map);
 
             var listings = @json($featuredListings);
