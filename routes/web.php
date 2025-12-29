858d4a0bb328ae2af007fb7a5316ae61bdc95246
Arben Mehmetaj <amehmetaj@gmail.com>
2025-12-29 21:47:25 +0100

    activate

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return view('temp');});
-
 /*
+Route::get('/', function () {return view('temp');});
+*/
 
 Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
 Route::get('/search', [App\Http\Controllers\ListingController::class, 'search'])->name('search');
@@ -37,5 +37,3 @@ Route::post('/listings/{id}/reviews', [App\Http\Controllers\ReviewController::cl
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-*/
