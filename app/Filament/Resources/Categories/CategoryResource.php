6d6e5a618d383cddba9e9f4e00cadf7999b6647a
Arben Mehmetaj <amehmetaj@gmail.com>
2026-03-20 11:37:37 +0100

    fix filamaent categories

--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -27,7 +27,7 @@ class CategoryResource extends Resource
                 \Filament\Forms\Components\TextInput::make('name')
                     ->required()
                     ->live(onBlur: true)
-                    ->afterStateUpdated(fn (string $operation, $state, \Filament\Forms\Set $set) => $operation === 'create' ? $set('slug', \Illuminate\Support\Str::slug($state)) : null),
+                    ->afterStateUpdated(fn (string $operation, $state, \Filament\Schemas\Components\Utilities\Set $set) => $operation === 'create' ? $set('slug', \Illuminate\Support\Str::slug($state)) : null),
                 \Filament\Forms\Components\TextInput::make('slug')
                     ->required()
                     ->unique(ignoreRecord: true),
